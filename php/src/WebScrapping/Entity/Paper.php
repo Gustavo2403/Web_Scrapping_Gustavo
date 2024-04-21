14fb8ab45b0b9ce63c8992aa1d8d74c8cb6d06cb
Gustavo Rocha Moreira <gustavormoreiraflamengo@gmail.com>
2024-04-21 09:24:01 -0300

    feat: definição da classe Paper e seus métodos

--- php/src/WebScrapping/Entity/Paper.php
+++ php/src/WebScrapping/Entity/Paper.php
@@ -39,6 +39,26 @@ class Paper {
    * Builder.
    */
   public function __construct($id, $title, $type, $authors = []) {
+    $this->id = $id;
+    $this->title = $title;
+    $this->type = $type;
+    $this->authors = $authors;
+  }
+
+  public function getId() {
+    return $this->id;
+  }
+
+  public function getTitle() {
+    return $this->title;
+  }
+
+  public function getType() {
+    return $this->type;
+  }
+
+  public function getPersons() {
+    return $this->authors;
   }
 
 }
