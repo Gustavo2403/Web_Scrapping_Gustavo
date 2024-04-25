89dd85c62727a690e464ce16ac2f721b45465da2
Gustavo Rocha Moreira <gustavormoreiraflamengo@gmail.com>
2024-04-25 13:39:30 -0300

    :feat: arquivo person organizado

--- php/src/WebScrapping/Entity/Person.php
+++ php/src/WebScrapping/Entity/Person.php
@@ -9,27 +9,48 @@ class Person {
 
   /**
    * Person name.
+   *
+   * @var string
    */
   public string $name;
 
   /**
    * Person institution.
+   *
+   * @var string
    */
   public string $institution;
 
   /**
    * Builder.
+   *
+   * @param string $name
+   *   The person's name.
+   * @param string $institution
+   *   The person's institution.
    */
   public function __construct($name, $institution) {
     $this->name = $name;
     $this->institution = $institution;
   }
 
-  public function getName(){
+  /**
+   * Get the person's name.
+   *
+   * @return string
+   *   The person's name.
+   */
+  public function getName() {
     return $this->name;
   }
 
-  public function getInstitution(){
+  /**
+   * Get the person's institution.
+   *
+   * @return string
+   *   The person's institution.
+   */
+  public function getInstitution() {
     return $this->institution;
   }
 
